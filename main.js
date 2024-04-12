fc0311c4644dd8cf3801bc1d1b516afa90072744
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-04-12 22:40:16 +0200

    Reestructuring main.js

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 // We use CJS and not ESM
-const fastify = require("fastify")({ logger: true });
+const fastify = require("fastify")({ logger: { level: "info" } });
+const helmet = require("@fastify/helmet");
 
 require("dotenv").config();
 
-// Declare a route
-fastify.register(require("./src/routes/routes"));
+// no sé por qué las jodidas peticiiones piden un favicon. Con esto
+// evitamos en el log q ponga que no existe.
+fastify.addHook("onRequest", (request, reply, done) => {
+  if (request.url === "/favicon.ico") {
+    reply.code(204).send();
+  } else {
+    done();
+  }
+});
 
-fastify.listen({port: process.env.PORT}, (err) => {
+// We load the dbConnector
+fastify.register(require("./src/plugins/db"));
+
+// We declare a route
+fastify.register(require("./src/routes/general"), { prefix: "/general" });
+fastify.register(require("./src/routes/static"), { prefix: "/static" });
+
+// we use helmet
+// fastify.register(helmet);
+
+fastify.listen({ port: process.env.PORT }, (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
