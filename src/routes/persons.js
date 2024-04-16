ea7f7896b261b02029b0446f30e3e200a7b1f38c
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-04-16 12:55:09 +0200

    Transform data for being consumed by chartjs in frontend.

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -8,13 +8,16 @@ const {
   pipeline
 } = require("../queries/persons");
 
+const {createDataChart} = require('../utils/dataForChart');
+
 async function routes(fastify, options) {
   const persons = fastify.mongo.atlanto.db.collection("persons");
 
   fastify.get("/birthyears", async (request, reply) => {
     try {
       const result = await persons.aggregate(pipeline).toArray();
-      reply.status(200).send(result);
+      const data = createDataChart(result);
+      reply.status(200).send(data);
     } catch (error) {
       console.error(error);
       reply.status(500).send("error en el servidor o en la consulta");
