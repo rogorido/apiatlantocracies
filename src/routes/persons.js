a780852bc8ac50d539b9ca70ece8ec50f42f62c4
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:06:06 +0200

    Person by id and by place

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -30,6 +30,53 @@ async function routes(fastify, options) {
       reply.status(500).send("error en el servidor o en la consulta");
     }
   });
+
+  fastify.get("/personsbyplace/:place", async (request, reply) => {
+    const { place } = request.params;
+    try {
+      const result = await persons
+        .aggregate(personsbyplace.pipeline(place))
+        .toArray();
+      reply.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send("error en el servidor o en la consulta");
+    }
+  });
+
+  fastify.get("/personsbyid/:id", async (request, reply) => {
+    const { id } = request.params;
+
+    if (!id) {
+      return reply.status(500).send("error en el servidor o en la consulta");
+    }
+
+    try {
+      const result = await vpersons.findOne({ _id: new ObjectId(id) });
+
+      // we add details about years in relations, etc.
+      const persondetails = personDetails(result);
+
+      // we create a timeline
+      const personeventstimeline = createDataTimeline(persondetails);
+
+      return reply.status(200).send({ persondetails, personeventstimeline });
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send("error en el servidor o en la consulta");
+    }
+  });
+
+  fastify.get("/histbirths", async (request, reply) => {
+    try {
+      const result = await persons.aggregate(histBirth).toArray();
+      const data = createDataChartHistBirths(result);
+      reply.status(200).send({ result, chartData: data });
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send("error en el servidor o en la consulta");
+    }
+  });
 }
 
 module.exports = routes;
