b5a3e4b6cb42c50013b40d8ec972698e9cfb5124
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:06:39 +0200

    Main routes of persons

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -19,6 +19,18 @@ const {
  */
 async function routes(fastify, options) {
   const persons = fastify.mongo.atlanto.db.collection("persons");
+  // const vpersons = fastify.mongo.atlanto.db.collection("vistapersonas");
+  const vpersons = fastify.mongo.atlanto.db.collection("vistapersonascontodo");
+
+  fastify.get("/", async (request, reply) => {
+    try {
+      const result = await vpersons.find().toArray();
+      reply.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send("error en el servidor o en la consulta");
+    }
+  });
 
   fastify.get("/birthyears", async (request, reply) => {
     try {
