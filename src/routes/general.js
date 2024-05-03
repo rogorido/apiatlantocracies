6536ca2de958e41dc4ad746aa5fdb990c6adbbc7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:08:35 +0200

    Different changes.

--- src/routes/general.js
+++ src/routes/general.js
@@ -4,12 +4,9 @@
  * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
  */
 
-const {
-  eventtypes,
-  positionstypes,
-  relationstypes,
-  titlestypes,
-} = require("../queries/general");
+const q = require("../queries/general");
+
+const { createDataChartGenders } = require("../utils/dataForChart");
 
 async function routes(fastify, options) {
   const persons = fastify.mongo.atlanto.db.collection("persons");
@@ -49,7 +46,7 @@ async function routes(fastify, options) {
 
   fastify.get("/eventtypes", async (request, reply) => {
     try {
-      const result = await persons.aggregate(eventtypes).toArray();
+      const result = await persons.aggregate(q.eventtypes).toArray();
       reply.status(200).send(result);
     } catch (error) {
       console.error(error);
@@ -59,7 +56,7 @@ async function routes(fastify, options) {
 
   fastify.get("/positionstypes", async (request, reply) => {
     try {
-      const result = await persons.aggregate(positionstypes).toArray();
+      const result = await persons.aggregate(q.positionstypes).toArray();
       reply.status(200).send(result);
     } catch (error) {
       console.error(error);
@@ -69,7 +66,7 @@ async function routes(fastify, options) {
 
   fastify.get("/relationstypes", async (request, reply) => {
     try {
-      const result = await persons.aggregate(relationstypes).toArray();
+      const result = await persons.aggregate(q.relationstypes).toArray();
       reply.status(200).send(result);
     } catch (error) {
       console.error(error);
@@ -79,102 +76,12 @@ async function routes(fastify, options) {
 
   fastify.get("/titlestypes", async (request, reply) => {
     try {
-      const result = await persons.aggregate(titlestypes).toArray();
+      const result = await persons.aggregate(q.titlestypes).toArray();
       reply.status(200).send(result);
     } catch (error) {
       console.error(error);
       reply.status(500).send("error en el servidor o en la consulta");
     }
-  });
-
-  fastify.get("/profundidadotro", async (request, reply) => {
-    // const prof = parseInt(request.params.prof);
-    const { operador } = request.query;
-    const prof = parseInt(request.query.prof);
-    // console.info(operador, prof);
-
-    try {
-      let filtro = {};
-
-      if (operador === "mayor") {
-        filtro = { depth: { $gt: prof } };
-      }
-
-      if (operador === "menor") {
-        filtro = { depth: { $lt: prof } };
-      }
-
-      // console.log(filtro);
-      const result = await colships.countDocuments(filtro);
-      reply.status(200).send({ total: result });
-    } catch (error) {
-      console.error(error);
-      reply.status(500).send("error en el servidor o en la consulta");
-    }
-  });
-
-  fastify.get("/profundidadcontar", async (request, reply) => {
-    const result = await colships
-      .find({ depth: { $exists: true, $ne: "" } })
-      .count();
-    return { total: result };
-  });
-
-  fastify.get("/mediames", async (request, reply) => {
-    const result = await colweather
-      .aggregate([
-        {
-          $match: { "airTemperature.value": { $ne: 999.9 } },
-        },
-        {
-          $group: {
-            _id: { $hour: "$ts" },
-            avgTemp: { $avg: "$airTemperature.value" },
-          },
-        },
-        { $sort: { _id: 1 } },
-      ])
-      .toArray();
-    // return result;
-    reply.status(200).send(result);
-  });
-
-  fastify.get("/estaciones", async (request, reply) => {
-    const result = await colweather
-      .aggregate([
-        {
-          $match: {
-            callLetters: {
-              $ne: "99999",
-            },
-          },
-        },
-        {
-          $group: {
-            _id: "$callLetters",
-            total: {
-              $sum: 1,
-            },
-          },
-        },
-        {
-          $sort: {
-            total: -1,
-          },
-        },
-      ])
-      .toArray();
-    reply.status(200).send(result);
-  });
-
-  fastify.get("/profundidadmedia", async (request, reply) => {
-    const result = await colships
-      .aggregate([
-        { $match: { depth: { $exists: true, $ne: "" } } },
-        { $group: { _id: null, avgProf: { $avg: "$depth" } } },
-      ])
-      .toArray();
-    return result;
   });
 }
 
