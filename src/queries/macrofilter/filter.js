0cd31691478fe46afc394dbb1504c3d15a2381ec
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:04:37 +0200

    Macrofilter : manage param from url

---
+++ src/queries/macrofilter/filter.js
@@ -0,0 +1,33 @@
+const pipeline = (filter) => {
+  if (filter.place) {
+    filter.$or = [
+      { placebirth: filter.place },
+      { placedeath: filter.place },
+      { "events.placeEv": filter.place },
+      { "positions.placePos": filter.place },
+      { "relations.placeRel": filter.place },
+    ];
+    delete filter.place;
+  } else {
+    delete filter.$or;
+  }
+
+  if (filter.tiposPositions) {
+    filter.tiposPositions = { $in: filter.tiposPositions };
+  }
+
+  if (filter.tiposRelations) {
+    filter.tiposRelations = { $in: filter.tiposRelations };
+  }
+
+  if (filter.histBirths) {
+    filter.histBirth = { $in: filter.histBirths };
+    delete filter.histBirths;
+  }
+
+  //console.log(JSON.stringify(filter, null, 2));
+
+  return filter;
+};
+
+module.exports = { pipeline };
