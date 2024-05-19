a5804ef6f0e80596be0308dd34afb8a38539a75f
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-19 22:24:05 +0200

    Checking for relations before creating graph of relations...

--- src/utils/personNetwork.js
+++ src/utils/personNetwork.js
@@ -2,27 +2,33 @@
 // Cogemos por ahora solo las personas de las relaciones
 const createDataPersonsNetwork = (data, originalperson) => {
   let nodes = [];
+  let edges = [];
   nodes.push({
     id: 0,
     label: originalperson,
     shape: "diamond",
   });
 
-  data.map((item, idx) => {
-    nodes.push({
-      id: idx + 1,
-      label: item.namePerson,
-      shape: "circle",
+  if (Array.isArray(data) && data.length > 0) {
+    data.map((item, idx) => {
+      nodes.push({
+        id: idx + 1,
+        label: item.namePerson,
+        shape: "circle",
+      });
+      // NOTE: why do I need this?
+      return {};
     });
-    return {};
-  });
 
-  const edges = data.map((item) => {
-    return {
-      from: nodes.findIndex((node) => node.label === item.namePerson),
-      to: 0,
-    };
-  });
+    data.map((item) => {
+      edges.push({
+        from: nodes.findIndex((node) => node.label === item.namePerson),
+        to: 0,
+      });
+      // NOTE: why do I need this? Again...
+      return {};
+    });
+  }
 
   return { nodes: nodes, edges: edges };
 };
